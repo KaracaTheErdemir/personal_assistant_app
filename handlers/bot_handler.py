c90306122e43b62f4cdf033ea399771207ecc7d0
YuceSezar33 <karacasama@gmail.com>
2024-09-10 19:42:57 +0300

    proper command direction is added

--- handlers/bot_handler.py
+++ handlers/bot_handler.py
@@ -11,23 +11,13 @@ from telegram.ext import (
     filters,
     Application,
 )
-sys.path.append(os.path.abspath('/Users/hermestrismegistus/personal_assistant_app'))
+#sys.path.append(os.path.abspath('/Users/hermestrismegistus/personal_assistant_app'))
 from config_manager import get_telegram_config
-from handlers.command_handler import list_entries, new_entry, report_entries, track_entry
+from handlers.command_handler import list_expenses, list_friends, list_habits, list_meetings, list_todos, new_expense, new_friend, new_habit, new_meeting, new_todo, track_habit, track_todo, update_expense, update_friend, update_habit, update_meeting, update_todo
 
 config = get_telegram_config()
 TELEGRAM_TOKEN = config['token']
 CHAT_ID = config['chat_id']
-
-# Handler for the /start command
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello! This is your bot.")
-    context.user_data['started'] = True
-
-# Handler for the /cancel command
-async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Cancelled.")
-    context.user_data['started'] = False
 
 async def send_message(message):
     # Create a Bot instance
@@ -54,10 +44,9 @@ async def start_bot():
 
         app = Application.builder().token(TELEGRAM_TOKEN).build()
         # Add handlers to the Application
-        app.add_handler(CommandHandler('cancel', cancel))
-        app.add_handler(CommandHandler('start', start))
+        app.add_handler(MessageHandler(filters.Command, handle_command))
         #TODO: change this to reading command lines instead
-        app.add_handler(MessageHandler(filters.Text(), handle_message))
+        #app.add_handler(MessageHandler(filters.Text(), handle_message))
         logging.info("Starting the bot")
 
         await app.initialize()
@@ -65,8 +54,6 @@ async def start_bot():
         await app.updater.start_polling()
     except Exception as e:
             logging.error(f"An error occurred in start_bot: {type(e).__name__} - {str(e)}")
-
-from telegram.ext import CommandHandler
 
 async def handle_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Extract the command and its arguments
@@ -77,21 +64,65 @@ async def handle_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         # Split the message into command and option
         parts = update.message.text.split()
         
-        if len(parts) < 2 or not parts[1].startswith('-'):
+        if len(parts) < 2:
             return "Invalid command format. Use an option starting with '-' after the command."
         
         command = parts[0]
         option = parts[1]
-
+        payload = parts
         # Route the command and option to the appropriate function
+
         if command == '/new':
-            return new_entry(option)
+            if option == 'friend':
+                return new_friend(parts)
+            elif option == 'meeting':
+                return new_meeting(parts)
+            elif option == 'expense':
+                return new_expense(parts)
+            elif option == 'habit':
+                return new_habit(parts)
+            elif option == 'plan':
+                return new_todo(parts)
+            else:
+                return "Invalid option for /new."
+
+        elif command == '/update':
+            if option == 'friend':
+                return update_friend(parts)
+            elif option == 'meeting':
+                return update_meeting(parts)
+            elif option == 'expense':
+                return update_expense(parts)
+            elif option == 'habit':
+                return update_habit(parts)
+            elif option == 'todo':
+                return update_todo(parts)
+            else:
+                return "Invalid option for /update."
+
         elif command == '/track':
-            return track_entry(option)
+            if option == 'habit':
+                return track_habit(parts)
+            elif option == 'plan':
+                return track_todo(parts)
+            else:
+                return "Invalid option for /track."
+
         elif command == '/list':
-            return list_entries(option)
-        elif command == '/report':
-            return report_entries(option)
+            if option == 'friends':
+                return list_friends(parts)
+            elif option == 'meetings':
+                return list_meetings(parts)
+            elif option == 'habits':
+                return list_habits(parts)
+            elif option == 'plans':
+                return list_todos(parts)
+            elif option == 'expenses':
+                return list_expenses(parts)
+            else:
+                return "Invalid option for /list."
+
+
         else:
             return "Unknown command."
     except Exception as e:
