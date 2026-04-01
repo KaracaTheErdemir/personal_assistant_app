7ff8fd74628084269eb74fcad5e53456050c7eaf
KaracaTheErdemir <karacasama@gmail.com>
2026-04-01 23:58:36 +0300

    sql alchemy update

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import asyncio
+from db import engine, Base
+import models  # VERY IMPORTANT
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -6,8 +8,12 @@ from handlers.bot_handler import start_bot
 
 app = Flask(__name__)
 
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+    
 def main():
     # asyncio.run(start_bot())
+    create_tables()
     loop = asyncio.get_event_loop()
     loop.create_task(start_bot())
     loop.run_forever()
