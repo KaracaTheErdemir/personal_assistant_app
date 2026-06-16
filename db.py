f551836d6c3b9779205c8716d1e8dd1935ffdce1
KaracaTheErdemir <karacasama@gmail.com>
2026-06-16 03:10:23 +0300

    db env variables added

--- db.py
+++ db.py
@@ -1,12 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from config_manager import get_database_config
-
-cfg = get_database_config()
 
 DATABASE_URL = (
-    f"postgresql+psycopg://{cfg['user']}:{cfg['password']}"
-    f"@{cfg['host']}:{cfg['port']}/{cfg['database']}"
+    f"postgresql+psycopg2://"
+    f"{os.getenv('POSTGRES_USER')}:"
+    f"{os.getenv('POSTGRES_PASSWORD')}@"
+    f"{os.getenv('DB_HOST')}:5432/"
+    f"{os.getenv('POSTGRES_DB')}"
 )
 
 engine = create_engine(DATABASE_URL, echo=True)
